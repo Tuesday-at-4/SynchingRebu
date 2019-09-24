9b77c76aade7939de178814e9da9d52c82b1d696
willgermain <wdgermain3834@eagle.fgcu.edu>
2019-09-24 15:39:36 -0400

    The time is 2:29pm in Texas.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Branch Deez...");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
@@ -19,4 +19,5 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
 }

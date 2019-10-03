b6f7b0ba6b0e92d9f25d706e55781f35cd364289
Mikeacino <32168280+Mikeacino@users.noreply.github.com>
2019-10-03 15:02:24 -0400

    Most recent version.

--- src/sample/Screen_Main_Controller.java
+++ src/sample/Screen_Main_Controller.java
@@ -1,8 +1,11 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 
 public class Screen_Main_Controller {
@@ -17,8 +20,17 @@ public class Screen_Main_Controller {
   }
 
   @FXML
-  private void handleTestButton() {
-    System.out.println("Heading to Screen One!");
+  private void handleTestButton() throws Exception{
+    System.out.println("Do Something!");
+    Stage temp = Main.getPrimaryStage();
+    Parent rootOne = FXMLLoader.load(getClass().getResource("Screen_One_Controller.fxml"));
+    //Scene secondScene = new Scene(rootOne,400,400);
+    //initialize();
+    //Main.start(temp, rootOne);
+    //temp.setScene(secondScene);
+
+
+    //Main.getPrimaryStage().setScene(secondScene);
     //initialize();
   }
 }
--- src/sample/Screen_One_Controller.java
+++ src/sample/Screen_One_Controller.java
@@ -1,7 +1,12 @@
 package sample;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
 public class Screen_One_Controller {
-public void initialize(){
-  System.out.println("Welcome to Screen One!");
-}
+  public void initialize() throws  Exception{
+
+    System.out.println("Welcome to Screen One!");
+  }
 }

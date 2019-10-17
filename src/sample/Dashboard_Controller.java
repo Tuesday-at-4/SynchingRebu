fd9e3fa90e284664175cea628844aeb069fb01cc
Mikeacino <32168280+Mikeacino@users.noreply.github.com>
2019-10-17 15:13:57 -0400

    Removed Dashboard controller class and added barebones buttons in Dashboard.fxml

--- src/sample/Dashboard_Controller.java
+++ src/sample/Dashboard_Controller.java
@@ -1,37 +1,3 @@
 package sample;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-
-public class Dashboard_Controller {
-
-    @FXML
-    private AnchorPane anchorDashboard;
-
-        @FXML
-        private HBox hBoxDashboard;
-
-        @FXML
-        private Button btnAccountSum;
-
-        @FXML
-        private Button btnDriver;
-
-        @FXML
-        private Button btnPassenger;
-
-    //Label introLabel = new Label("Hello");
-
-    public void initialize(){
-        Button btnAccountSum = new Button("Account Summary");
-        Button btnDriver = new Button("Driver");
-        Button btnPassenger = new Button("Passenger");
-        HBox hBoxDashboard = new HBox();
-        hBoxDashboard.getChildren().addAll(btnAccountSum, btnDriver, btnPassenger);//,btnDriver,btnPassenger);
-        anchorDashboard.getChildren().add(hBoxDashboard);
-    }
-}
+public class Dashboard_Controller {}

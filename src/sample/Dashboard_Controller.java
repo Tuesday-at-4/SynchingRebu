d982629d1734ba49f767f9581d4c4a167f3221fc
Mikeacino <32168280+Mikeacino@users.noreply.github.com>
2019-10-18 19:22:41 -0400

    Added The Driver controller, Driver FXML, Login Controller, and Login FXML

--- src/sample/Dashboard_Controller.java
+++ src/sample/Dashboard_Controller.java
@@ -1,41 +1,23 @@
 package sample;
 
-import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public class Dashboard_Controller {
-
     public void handlePassengerButton(ActionEvent event){
-        createNewScene(event,"PAssenger.FXML");
+        Main.createNewScene(event,"Passenger.FXML");
     }
 
     @FXML
     private void handleAccountButton(ActionEvent event){
-        createNewScene(event,"AccountSummary.FXML");
+        Main.createNewScene(event,"AccountSummary.FXML");
     }
 
     @FXML
     private void handleDriverButton(ActionEvent event){
-        createNewScene(event,"Driver.FXML");
+        Main.createNewScene(event,"Driver.FXML");
     }
 
-    private void createNewScene(ActionEvent event, String newFileFXML){
-        Parent newRoot = null;
-        try {
-            newRoot = FXMLLoader.load(getClass().getResource(newFileFXML));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Scene newScene = new Scene(newRoot, 500, 500);
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(newScene);
-        window.show();
-    }
+
 
 }
---
+++ src/sample/Driver_Controller.java
@@ -0,0 +1,96 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.MouseEvent;
+
+public class Driver_Controller<Avatar> {
+
+  @FXML
+  private Avatar MyAvatar;
+
+  @FXML
+  private Label Rating;
+
+  @FXML
+  private Label label_Name;
+
+  @FXML
+  private Label label_Age;
+
+  @FXML
+  private TextField Text_Name;
+
+  @FXML
+  private TextField Text_Age;
+
+  @FXML
+  private TableView<?> TableView;
+
+  @FXML
+  private TextField getManufacturer;
+
+  @FXML
+  private TextField getModel;
+
+  @FXML
+  private TextField getYear;
+
+  @FXML
+  private Button New_Button;
+
+  @FXML
+  private Button Edit_Button;
+
+  @FXML
+  private Button Delete_Button;
+
+  @FXML
+  private TableView<?> Active_Rides;
+
+  @FXML
+  void CreateNew(MouseEvent event) {
+
+  }
+
+  @FXML
+  void Delete_Item(MouseEvent event) {
+
+  }
+
+  @FXML
+  void DisplayVehicles(ActionEvent event) {
+
+  }
+
+  @FXML
+  void Edit_Item(MouseEvent event) {
+
+  }
+
+  @FXML
+  void add_ActiveRides(ActionEvent event) {
+
+  }
+
+  @FXML
+  void add_Manu(InputMethodEvent event) {
+
+  }
+
+  @FXML
+  void add_Model(InputMethodEvent event) {
+
+  }
+
+  @FXML
+  void add_Year(InputMethodEvent event) {
+
+  }
+
+}

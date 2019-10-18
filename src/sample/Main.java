8c9445b2a60e074551f37c668da5e306778f4fd8
Ben cano <54292636+BC4764@users.noreply.github.com>
2019-10-17 22:23:41 -0400

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,17 +7,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(Main.class, args);
+    }
+    @Override
+    public void start(Stage Stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Stage.setTitle("Driver");
+        Stage.setScene(new Scene(root, 1000, 685));
+        Stage.show();
     }
 
 }

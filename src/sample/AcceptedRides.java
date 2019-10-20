2a74b3d587a67db91be71ba903ae6039e8ce6da1
Ben cano <54292636+BC4764@users.noreply.github.com>
2019-10-20 16:26:07 -0400

    Update AcceptedRides.java

--- src/sample/AcceptedRides.java
+++ src/sample/AcceptedRides.java
@@ -3,24 +3,28 @@ package sample;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 
 public class AcceptedRides {
 
-  private SimpleStringProperty Passenger2;
+
+  private String Passenger2;
   private LocalTime current_Time;
   private LocalDate current_Date;
 
   public AcceptedRides(String Passenger2, LocalTime currentTime, LocalDate currentDate) {
-    this.Passenger2 = new SimpleStringProperty(Passenger2);
+    this.Passenger2 = Passenger2;
     this.current_Date = currentDate;
     this.current_Time = currentTime;
   }
 
   public String getPassenger2() {
-    return Passenger2.get();
+    return Passenger2;
   }
 
-  public void setPassenger2(SimpleStringProperty Passenger2) {
+  public void setPassenger2(String Passenger2) {
     this.Passenger2 = Passenger2;
   }
 
@@ -39,5 +43,6 @@ public class AcceptedRides {
   public void setCurrent_Time(LocalTime currentTime) {
     this.current_Time = currentTime;
   }
+
 }
 

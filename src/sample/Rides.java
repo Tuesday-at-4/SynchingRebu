931279c1101ce3e55689ab4170fe4e7561546abe
Ben cano <54292636+BC4764@users.noreply.github.com>
2019-10-20 14:36:30 -0400

    Update Rides.java

--- src/sample/Rides.java
+++ src/sample/Rides.java
@@ -5,18 +5,20 @@ import java.time.LocalTime;
 import javafx.beans.property.SimpleStringProperty;
 
 public class Rides {
-private SimpleStringProperty passengerName;
-private LocalTime time_OfRide;
-private LocalDate date_OfRide;
 
-public Rides(String passengerName, LocalTime time, LocalDate Date){
-  this.passengerName = new SimpleStringProperty(passengerName);
-  this.date_OfRide = Date;
-  this.time_OfRide = time;
-}
-public String getPassengerName(){
-  return passengerName.get();
-}
+  private SimpleStringProperty passengerName;
+  private LocalTime time_OfRide;
+  private LocalDate date_OfRide;
+
+  public Rides(String passengerName, LocalTime time, LocalDate Date) {
+    this.passengerName = new SimpleStringProperty(passengerName);
+    this.date_OfRide = Date;
+    this.time_OfRide = time;
+  }
+
+  public String getPassengerName() {
+    return passengerName.get();
+  }
 
   public void setPassengerName(SimpleStringProperty passengerName) {
     this.passengerName = passengerName;
